28365adffe65f8c22022e604d0eabdd185209c5d
arunsoni26 <106370751+arunsoni26@users.noreply.github.com>
2025-10-12 22:44:41 +0530

    add some migrations

---
+++ database/migrations/2025_10_12_125907_create_products_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('sku')->nullable();
+            $table->foreignId('category_id')->constrained('product_categories')->onDelete('cascade');
+            $table->foreignId('brand_id')->nullable()->constrained('brands')->onDelete('set null');
+            $table->text('short_description')->nullable();
+            $table->longText('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->decimal('discount_price', 10, 2)->nullable();
+            $table->integer('stock_quantity')->default(0);
+            $table->string('weight')->nullable();
+            $table->string('flavor_notes')->nullable();
+            $table->string('origin')->nullable();
+            $table->string('image')->nullable();
+            $table->json('gallery_images')->nullable();
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('status')->default(1); // 1 = active
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
